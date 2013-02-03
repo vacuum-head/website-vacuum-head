5b40840b4c78bfec3cdbe1e88741c5e4cd86ef99
Pratyush Verma <pratyush.verma@flipkart.com>
2013-02-04 01:49:03 +0530

    root to script

--- deploy.php
+++ deploy.php
@@ -22,7 +22,7 @@
 	$output = '';
 	foreach($commands AS $command){
 		// Run it
-		$tmp = shell_exec("sudo " . $command);
+		$tmp = shell_exec($command);
 		// Output
 		$output .= "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #729FCF;\">{$command}\n</span>";
 		$output .= htmlentities(trim($tmp)) . "\n";
